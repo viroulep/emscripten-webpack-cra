f39c1c8c243f0805ef2a05458d0d3095c3d12ab3
Philippe Virouleau <pvirouleau@quarkslab.com>
2022-09-22 12:06:41 +0200

    Update this example to latest versions
    There have been a few significant changes with the latest versions of
    emscripten, webpack, and react: this is now significantly easier to
    import wasm!
    No need for craco anymore, and the tricks to make it work with webpack
    are pretty much gone (except for the locateFile one).

--- src/App.js
+++ src/App.js
@@ -3,7 +3,23 @@ import './App.css';
 // this just loads stuff to load the wasm
 import hello from './wasm/hello';
 
-const Loaded = ({ wasm }) => <button onClick={() => console.log(wasm.func(1,6))}>Click me</button>;
+const action = (wasm) => {
+  // Try a simple function call
+  console.log(wasm.myadd(1, 2));
+  const vectorOfPoints = new wasm.VectorPoint();
+  // Push a couple of points:
+  vectorOfPoints.push_back({ x: 1, y: 2 });
+  vectorOfPoints.push_back({ x: 4, y: 5 });
+  // Accumulate them
+  const { x, y } = wasm.accumulatePoints(vectorOfPoints);
+  if (x === 5 && y === 7) {
+    console.log("Accumulation worked");
+  } else {
+    console.log("Accumulation did not work");
+  }
+};
+
+const Loaded = ({ wasm }) => <button onClick={() => action(wasm)}>Click me</button>;
 
 const Unloaded = ({ loading, loadWasm }) => {
   return loading ? (
@@ -17,18 +33,11 @@ const App = () => {
   const [loading, setLoading] = useState(false);
   const [wasm, setWasm] = useState(null);
   const loadWasm = async () => {
-    try {
-      setLoading(true);
-      const wasm = hello({
-        onRuntimeInitialized: () => {
-          setWasm(wasm);
-        },
-        // This overrides the default path used by the wasm/hello.js wrapper
-        locateFile: () => require("./wasm/hello.wasm"),
-      });
-    } finally {
-      setLoading(false);
-    }
+    setLoading(true);
+    hello({
+      // This overrides the default path used by the wasm/hello.mjs wrapper
+      locateFile: () => require("./wasm/hello.wasm"),
+    }).then((wasm) => { setWasm(wasm); setLoading(false); });
   };
 
   return (
